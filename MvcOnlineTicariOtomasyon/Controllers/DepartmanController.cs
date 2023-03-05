3978b6bbacc01b62e88f734cc24bb33f4b476bbe
BULUTS <BULUTS@BULUTS>
2023-03-05 21:10:30 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Controllers/DepartmanController.cs
@@ -0,0 +1,51 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        Context c = new Context();
+        // GET: Departman
+        public ActionResult Index()
+        {
+            var degerler = c.Departmans.Where(x=>x.Durum == true).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman d)
+        {
+            c.Departmans.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanSil(int id)
+        {
+            var dep = c.Departmans.Find(id);
+            dep.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanGetir(int id)
+        {
+            var dpt = c.Departmans.Find(id);
+            return View("DepartmanGetir",dpt);
+        }
+        public ActionResult DepartmanGuncelle(Departman p)
+        {
+            var dept = c.Departmans.Find(p.DepartmanID);
+            dept.DepartmanAd = p.DepartmanAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
