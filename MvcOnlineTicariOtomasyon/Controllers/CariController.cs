4961def24b819bb0c60bca74a440b975d20754d8
BULUTS <BULUTS@BULUTS>
2023-03-08 18:56:39 +0300

    58. Carinin Yaptığı Satışları Listeleme

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,63 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        Context c = new Context();
+        // GET: Car
+        public ActionResult Index()
+        {
+            var degerler = c.Carilers.Where(x=>x.Durum == true).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniCari()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniCari(Cariler p)
+        {
+            p.Durum = true;
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariSil(int id)
+        {
+            var cari = c.Carilers.Find(id);
+            cari.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariGetir(int id)
+        {
+            var cari = c.Carilers.Find(id);
+            return View("CariGetir", cari);
+        }
+        public ActionResult CariGuncelle(Cariler p)
+        {
+            var cari = c.Carilers.Find(p.CariID);
+            cari.CariAd = p.CariAd;
+            cari.CariSoyad = p.CariSoyad;
+            cari.CariSehir = p.CariSehir;
+            cari.CariMail = p.CariMail;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MusteriSatis(int id)
+        {
+            var degerler = c.satisHarekets.Where(x => x.CariID == id).ToList();
+            var cr = c.Carilers.Where(x => x.CariID == id).Select(y => y.CariAd + " " + y.CariSoyad).FirstOrDefault();
+            ViewBag.cari = cr;
+            return View(degerler);
+        }
+    }
+}
