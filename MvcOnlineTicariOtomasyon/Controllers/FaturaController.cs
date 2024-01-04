c1fabd5cb7918ee4c437c73fc695a2f26963ce9f
DEV <DEV@DESKTOP-8VP4IIT>
2024-01-04 15:05:45 +0300

    77. Yeni Fatura Girişi

---
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var liste = c.Faturalars.ToList();
+            return View(liste);
+        }
+        [HttpGet]
+        public ActionResult FaturaEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FaturaEkle(Faturalar f)
+        {
+            c.Faturalars.Add(f);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
