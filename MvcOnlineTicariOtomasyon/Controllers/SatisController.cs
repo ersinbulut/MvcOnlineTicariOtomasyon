486e71fce3db23e447597da1a5e0b5c3ef930eb8
DEV <DEV@DESKTOP-8VP4IIT>
2024-01-04 11:59:18 +0300

    74. Tabloyu Pdf ve Excel'e Dönüştürme

--- MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -52,5 +52,55 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult SatisGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.UrunID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.CariID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.PersonelID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+            var deger = c.satisHarekets.Find(id);
+            return View("SatisGetir", deger);
+        }
+
+        public ActionResult SatisGuncelle(SatisHareket p)
+        {
+            var deger = c.satisHarekets.Find(p.SatisID);
+            deger.CariID = p.CariID;
+            deger.Adet = p.Adet;
+            deger.Fiyat = p.Fiyat;
+            deger.PersonelID = p.PersonelID;
+            deger.Tarih = p.Tarih;
+            deger.ToplamTutar = p.ToplamTutar;
+            deger.UrunID = p.UrunID;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SatisDetay(int id)
+        {
+            var degerler = c.satisHarekets.Where(x => x.SatisID == id).ToList();
+            return View(degerler);
+        }
+
     }
 }
