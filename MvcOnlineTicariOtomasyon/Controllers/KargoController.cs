d0d48160570f79defcbe91067fefd3e7ac8fc4a7
Developer <Developer@DEV>
2025-12-16 14:39:26 +0300

    176. Kargo Detay Sayfası Tasarımı

---
+++ MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using PagedList;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class KargoController : Controller
+    {
+        Context c = new Context();
+        // GET: Kargo
+
+
+        public ActionResult Index(string search, int page = 1, int pageSize = 10)
+        {
+            var kargolar = c.KargoDetays.AsQueryable();
+
+            // 🔍 KARGO TAKİP NO + DİĞER ALANLAR
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                kargolar = kargolar.Where(x =>
+                    x.TakipKodu.Contains(search) ||   // ✅ KARGO TAKİP NO
+                    x.Alici.Contains(search) ||
+                    x.Personel.Contains(search)
+                );
+            }
+
+            ViewBag.CurrentSearch = search;
+            ViewBag.PageSize = pageSize;
+
+            var pagedList = kargolar
+                .OrderByDescending(x => x.Tarih)
+                .ToPagedList(page, pageSize);
+
+            return View(pagedList);
+        }
+
+
+    }
+}
